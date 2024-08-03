4b24b8436f6db3b173acfe535306e085459f88c5
Megafont <51346737+Megafont@users.noreply.github.com>
2024-08-03 15:31:48 -0500

    Finished Video #14
    + Added login page

---
+++ RunningGroupsWebApp/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
+using RunningGroupsWebApp.ViewModels;
+
+namespace RunningGroupsWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _UserManager;
+        private readonly SignInManager<AppUser> _SignInManager;
+        private readonly ApplicationDbContext _DbContext;
+
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext dbContext)
+        {
+            _UserManager = userManager;
+            _SignInManager = signInManager;
+            _DbContext = dbContext;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) 
+                return View(loginViewModel);
+
+            var user = await _UserManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                // We found the user, so check the password.
+                var passwordCheck = await _UserManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    // The password is correct, so sign the user in.
+                    var result = await _SignInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Races");
+                    }
+                }
+
+                // He said using TempData very much is a "design smell", and that normally you'd have a boolean in loginViewModel, such as loginViewModel.PasswordIsCorrect.
+                TempData["Error"] = "Wrong credentials. Please try again.";
+                return View(loginViewModel);
+            }
+
+            // The user was not found.
+            TempData["Error"] = "Wrong credentials. Please try again.";
+            return View(loginViewModel);
+
+        }
+    }
+}
---
+++ RunningGroupsWebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunningGroupsWebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email address is required.")]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
